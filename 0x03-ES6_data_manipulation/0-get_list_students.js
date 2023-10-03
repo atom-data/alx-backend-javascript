e1dcf8da12dfc62d18e1a0b3bd51a112d13bac31
atom-data <alatatomondi@gmail.com>
2023-10-03 17:32:06 +0000

    Array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function () {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { firstName: 'James', id: 2, location: 'Columbia' },
+    { firstName: 'Serena', id: 5, location: 'San Francisco' },
+  ];
+}
